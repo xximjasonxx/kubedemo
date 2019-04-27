6a806bb36aea11bf2494214bd4cb89cecb4d14e4
Jason Farrell <imjason@gmail.com>
2019-04-27 12:01:00 -0500

    Updates to add listener

--- StockPriceApi/Listener/StockPriceListener.cs
+++ StockPriceApi/Listener/StockPriceListener.cs
@@ -3,28 +3,68 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace StockPriceApi.Listener
 {
     public class StockPriceListener : IHostedService
     {
-        private Timer _timer;
+        private readonly string ExchangeName = "TheExchange";
+
+        private IConnection _connection;
+        private IModel _channel;
+        private EventingBasicConsumer _consumer;
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _timer = new Timer(Execute, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            _connection = BuildConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(
+                exchange: ExchangeName,
+                type: ExchangeType.Direct
+            );
+
+            var queueName = _channel.QueueDeclare().QueueName;
+            Console.WriteLine($"Queue: {queueName}");
+
+            _consumer = new EventingBasicConsumer(_channel);
+            _consumer.Received += HandleMessageReceive;
+            _consumer.Registered += (obj, ev) => Console.WriteLine("Registered");
 
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            if (_consumer != null)
+            {
+                _consumer.Received -= HandleMessageReceive;
+            }
+
+            _channel?.Dispose();
+            _connection?.Dispose();
+
+            return Task.CompletedTask;
         }
 
-        void Execute(object state)
+        void HandleMessageReceive(object sender, BasicDeliverEventArgs e)
         {
-            Console.WriteLine("Executing");
+            Console.WriteLine("Message Received");
+        }
+
+        IConnection BuildConnection()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "192.168.99.100",
+                Port = 30046,
+                UserName = "rabbit",
+                Password = "rabbit"
+            };
+
+            return factory.CreateConnection();
         }
     }
 }
