38facbc982146bbdd0ba49f350b0a7f2696ac1d9
Jason Farrell <imjason@gmail.com>
2019-04-27 21:02:41 -0500

    Initial Files created for web app

---
+++ stock-app/src/components/MainLanding.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const MainLanding = () => {
+    return (
+        <div>Hello World</div>
+    );
+};
+
+export default MainLanding;
---
+++ stock-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainLanding from './components/MainLanding';
+
+const App = () => (
+    <MainLanding />
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
