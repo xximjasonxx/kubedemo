e544e5944e7be9f2153db2af6958bfcb5f071330
Jason Farrell <imjason@gmail.com>
2019-04-27 21:18:47 -0500

    Updated app to use Redux

--- stock-app/src/components/MainLanding.jsx
+++ stock-app/src/components/MainLanding.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
 
-const MainLanding = () => {
+const MainLandingComponent = () => {
     return (
         <div>Hello World</div>
     );
 };
 
-export default MainLanding;
+const mapStateToProps = (state) => {
+    return {};
+};
+
+export default connect(mapStateToProps)(MainLandingComponent);
