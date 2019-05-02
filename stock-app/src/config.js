55cae86f8489f7d864d62e0ea37df3a00bf7147c
Jason Farrell <imjason@gmail.com>
2019-05-01 19:37:17 -0500

    Updates to remove extraneous resources from cluster

--- stock-app/src/config.js
+++ stock-app/src/config.js
@@ -1,13 +1,4 @@
 
-const local = {
+export default {
     API_HOSTNAME: "http://192.168.99.100:31000"
-};
-
-const cluster = {
-    API_HOSTNAME: "http://10.110.80.93"
 }
-
-const config = process.env.REACT_APP_STAGE === 'cluster' ? cluster : local;
-console.log(config);
-
-export default config;
